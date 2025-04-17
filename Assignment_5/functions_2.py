09398f4e0f0d64fe308463d31f77868f1041a24c
AlexDelBarco <s232518@dtu.dk>
2025-04-17 17:22:25 +0200

    Q521 done

--- Assignment_5/functions_2.py
+++ Assignment_5/functions_2.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import rainflow as rf
+from rainflow import count_cycles
 
 def load_csv_files_to_dict(folder_path):
 
@@ -14,15 +14,45 @@ def load_csv_files_to_dict(folder_path):
             dataframes[df_name] = pd.read_csv(file_path, sep=';')
     return dataframes
 
-def f(s, f_s, m):
-    '''
-    s is load signal in time series
-    f_s is the sampling frequency
-    m is the Wohler exponent
 
-    compute rainflow count of the signal s
-    DEL : 1-Hz Damage Equivalent Value
-    '''
+
+
+def compute_del(s, f_s, m):
+    """
+    Computes the 1-Hz Damage Equivalent Load (DEL)
+      for a given load signal, frequency and Wohler exponent.
+
+    Args:
+        s (array-like): Load signal in time series.
+        f_s (float): Sampling frequency of the signal (Hz).
+        m (float): Wohler exponent.
+
+    Returns:
+        float: The 1-Hz Damage Equivalent Load (DEL).
+    """
+    # Convert to NumPy array, drop NaN values, verify not an empty series
+    signal = np.array(s)
+    signal = signal[~np.isnan(signal)]
+
+    if len(signal) == 0:
+        print("Error: The signal is empty")
+        return None
+
+    # Perform rainflow counting
+    cycles = count_cycles(signal)
+
+    # Calculate the damage by cycle
+    damage = 0
+    for amplitude, count in cycles:
+        damage += (amplitude ** m) * count
+
+    # Compute the DEL
+    T = len(signal) / f_s  # Total duration of the signal in seconds
+    DEL = (damage / T) ** (1 / m)
+
+    return DEL
+
+
 
     # take dictionary, each timeseries, each MxA1, MyA1, MxR... count the N using rainflow
 
--- Assignment_5/main_AS05_2.py
+++ Assignment_5/main_AS05_2.py
@@ -14,3 +14,32 @@ timeseries_dict = fn.load_csv_files_to_dict(data_path)
 
 #%% Q5.2.0
 
+# Function created, it is written in functions_2.py called compute_del
+
+#f_s_Q520 = 1 # Hz
+
+#del_test = fn.compute_del(timeseries_dict['201701032150']['MxA1'], f_s_Q520, 3)
+
+#%% Q5.2.1
+
+# Sampling frequency for DEL calculation
+f_s_Q521 = 1 # Hz
+
+# Wohler exponents
+m_list = [3, 6, 9, 12]
+
+# Signals/loads timeseries for each timeseries
+s_litst = ['MxA1', 'MyA1', 'MxR', 'MyR', 'MzR', 'MTilt', 'MYaw', 'MzTT', 'MyTB', 'MxTB']
+
+# Timeseries: 21/03/2017; 14:30
+ts = '201703211430' 
+
+results_DEL = {}
+
+for m in m_list:
+    for s in s_litst:
+        # Compute DEL
+        del_value = fn.compute_del(timeseries_dict[ts][s], f_s_Q521, m)
+        results_DEL[f"{s}_m{m}"] = del_value
+
+#%% Q5.2.2 
